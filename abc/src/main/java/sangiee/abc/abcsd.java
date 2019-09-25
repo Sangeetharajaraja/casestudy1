5e08ba0bef3948e8838541172a0e54c4d2fe460e
Training_C2A.04.30 <Training_C2A.04.30@CDC2-D-49G1N62.dir.svc.accenture.com>
2019-09-25 17:45:45 +0530

    hey

---
+++ abc/src/main/java/sangiee/abc/abcsd.java
@@ -0,0 +1,11 @@
+
+package sangiee.abc;
+
+import io.cucumber.java.en.Given;
+
+public class abcsd {
+@Given("I enter 25 in the calculator")
+public void given() {
+	System.out.println("I enter 25 in the calculator");
+}
+}
